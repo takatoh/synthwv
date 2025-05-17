b2f976f6911483dd934bd4062129686c5e0279c6
takatoh <takatoh.m@gmail.com>
2025-05-17 19:07:22 +0900

    synthesizer/synthesizer.go(Synthesize): Fix.

--- synthesizer/synthesizer.go
+++ synthesizer/synthesizer.go
@@ -19,9 +19,12 @@ func New(dt float64, omega, phi []float64) *Synthesizer {
 func (s *Synthesizer) Synthesize(n int, a []float64) []float64 {
 	t := 0.0
 	y := make([]float64, n)
-	for i := range n {
-		y[i] = a[i] * math.Sin(s.omega[i]*t+s.phi[i])
-		t += s.dt
+	m := len(s.omega)
+	for j := range n {
+		for i := range m {
+			y[j] = a[i] * math.Sin(s.omega[i]*t+s.phi[i])
+			t += s.dt
+		}
 	}
 	return y
 }
