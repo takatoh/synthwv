43e4adfd7235861b508e18258df1f3da63054b68
takatoh <takatoh.m@gmail.com>
2025-05-17 18:42:28 +0900

    phase/phase.go: New.

---
+++ phase/phase.go
@@ -0,0 +1,14 @@
+package phase
+
+import (
+	"math"
+	"math/rand"
+)
+
+func RandomPhaseAngles(n int) []float64 {
+	phi := make([]float64, n)
+	for i := range n {
+		phi[i] = rand.Float64() * math.Pi
+	}
+	return phi
+}
