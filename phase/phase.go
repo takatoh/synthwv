9d2f94b749cde246b4bad8ca958862cedd69c230
takatoh <takatoh.m@gmail.com>
2025-05-17 20:36:23 +0900

    phase/phase.go(RandomPhaseAngles): Expand phi to 2*pi.

--- phase/phase.go
+++ phase/phase.go
@@ -8,7 +8,7 @@ import (
 func RandomPhaseAngles(n int) []float64 {
 	phi := make([]float64, n)
 	for i := range n {
-		phi[i] = rand.Float64() * math.Pi
+		phi[i] = rand.Float64() * 2.0 * math.Pi
 	}
 	return phi
 }
