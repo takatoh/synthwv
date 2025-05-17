85208bf4793fd3b7595d4a43f97aa2786eac864b
takatoh <takatoh.m@gmail.com>
2025-05-17 20:50:59 +0900

    synthesizer/synthesizer.go: Accept envelope function and apply to timehist.

--- synthesizer/synthesizer.go
+++ synthesizer/synthesizer.go
@@ -3,16 +3,18 @@ package synthesizer
 import "math"
 
 type Synthesizer struct {
-	dt    float64
-	omega []float64
-	phi   []float64
+	dt       float64
+	omega    []float64
+	phi      []float64
+	envelope func(float64) float64
 }
 
-func New(dt float64, omega, phi []float64) *Synthesizer {
+func New(dt float64, omega, phi []float64, envelope func(float64) float64) *Synthesizer {
 	p := new(Synthesizer)
 	p.dt = dt
 	p.omega = omega
 	p.phi = phi
+	p.envelope = envelope
 	return p
 }
 
@@ -24,6 +26,7 @@ func (s *Synthesizer) Synthesize(n int, a []float64) []float64 {
 		for i := range m {
 			y[j] += a[i] * math.Sin(s.omega[i]*t+s.phi[i])
 		}
+		y[j] = s.envelope(y[j])
 		t += s.dt
 	}
 	return y
