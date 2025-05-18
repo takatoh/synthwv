6cee5ef3c3585596d65ca2909dc16686118233e4
takatoh <takatoh.m@gmail.com>
2025-05-18 20:32:42 +0900

    iterator/iterator.go(Iterator): Member variable 'iter_limit'.

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -8,25 +8,26 @@ import (
 type Iterator struct {
 	synthesizer *synthesizer.Synthesizer
 	inspector   *inspector.Inspector
+	iter_limit  int
 }
 
-func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector) *Iterator {
+func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector, iter_limit int) *Iterator {
 	p := new(Iterator)
 	p.synthesizer = synthesizer
 	p.inspector = inspector
+	p.iter_limit = iter_limit
 	return p
 }
 
 func (itr *Iterator) Iterate(n int, amp []float64) []float64 {
 	var y []float64
-	iter_limit := 3
 	count := 0
 	for {
 		count += 1
 		y = itr.synthesizer.Synthesize(n, amp)
 		if itr.inspector.Inspect(y) {
 			break
-		} else if count == iter_limit {
+		} else if count == itr.iter_limit {
 			break
 		}
 	}
