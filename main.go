8d41c61106d20390c63f88c1aaad813cb7ad1bf2
takatoh <takatoh.m@gmail.com>
2025-09-20 22:25:04 +0900

    main.go: Fix help message.

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ Options:
 	}
 	optLength := flag.Float64("length", 60.0, "Time-history length(sec).")
 	optDt := flag.Float64("dt", 0.01, "DT")
-	optLevel := flag.Int("level", 2, "Specify level 1 or 2 (default to 2).")
+	optLevel := flag.Int("level", 2, "Specify level 1 or 2.")
 	flag.Parse()
 
 	dt := *optDt
