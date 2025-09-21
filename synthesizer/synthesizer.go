f75f6bbe2c290b59f19b073ef39be92b43f17014
takatoh <takatoh.m@gmail.com>
2025-09-21 18:43:22 +0900

    synthesizer/synthesizer.go(Synthesizer): Public varialb N as number of data, and not accept in Synthesize.

--- synthesizer/synthesizer.go
+++ synthesizer/synthesizer.go
@@ -3,6 +3,7 @@ package synthesizer
 import "math"
 
 type Synthesizer struct {
+	N        int
 	Dt       float64
 	omega    []float64
 	phi      []float64
@@ -18,11 +19,11 @@ func New(dt float64, omega, phi []float64, envelope func(float64) float64) *Synt
 	return p
 }
 
-func (s *Synthesizer) Synthesize(n int, a []float64) []float64 {
+func (s *Synthesizer) Synthesize(a []float64) []float64 {
 	t := 0.0
-	y := make([]float64, n)
+	y := make([]float64, s.N)
 	m := len(s.omega)
-	for j := range n {
+	for j := range s.N {
 		for i := range m {
 			y[j] += a[i] * math.Sin(s.omega[i]*t+s.phi[i])
 		}
