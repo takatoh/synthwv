f4e1a4316b322fb6bff66bdd8d2fe16ab8c6b3b3
takatoh <takatoh.m@gmail.com>
2025-09-21 18:02:22 +0900

    inspector/inspector.go: Support chagens of fitting.

--- inspector/inspector.go
+++ inspector/inspector.go
@@ -1,18 +1,21 @@
 package inspector
 
+import "github.com/takatoh/seismicwave"
+
 type Inspector struct {
-	tests []func([]float64) bool
+	tests []func(*seismicwave.Wave, float64) bool
+	h     float64
 }
 
-func New(tests []func([]float64) bool) *Inspector {
+func New(tests []func(*seismicwave.Wave, float64) bool, h float64) *Inspector {
 	p := new(Inspector)
 	p.tests = tests
 	return p
 }
 
-func (ins *Inspector) Inspect(y []float64) bool {
+func (ins *Inspector) Inspect(y *seismicwave.Wave) bool {
 	for _, test := range ins.tests {
-		if !(test(y)) {
+		if !(test(y, ins.h)) {
 			return false
 		}
 	}
