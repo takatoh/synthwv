ecbc112e9dab02539de388cbce9f09b970819bca
takatoh <takatoh.m@gmail.com>
2025-09-22 05:40:45 +0900

    iterator/iterator.go(Iterate): Use Synthesizer.N for number of data.

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -10,15 +10,13 @@ type Iterator struct {
 	synthesizer *synthesizer.Synthesizer
 	inspector   *inspector.Inspector
 	iter_limit  int
-	dt          float64
 }
 
-func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector, iter_limit int, dt float64) *Iterator {
+func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector, iter_limit int) *Iterator {
 	p := new(Iterator)
 	p.synthesizer = synthesizer
 	p.inspector = inspector
 	p.iter_limit = iter_limit
-	p.dt = dt
 	return p
 }
 
@@ -28,7 +26,7 @@ func (itr *Iterator) Iterate(amp []float64) []float64 {
 	for {
 		count += 1
 		y = itr.synthesizer.Synthesize(amp)
-		wave := seismicwave.Make("", itr.dt, y)
+		wave := seismicwave.Make("", itr.synthesizer.Dt, y)
 		if itr.inspector.Inspect(wave) {
 			break
 		} else if count == itr.iter_limit {
