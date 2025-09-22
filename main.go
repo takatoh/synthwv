0890b2ed008f5ab31fac4bbc8011985a944c5155
takatoh <takatoh.m@gmail.com>
2025-09-22 11:08:15 +0900

    -envelope option.

--- main.go
+++ main.go
@@ -32,7 +32,8 @@ Options:
 	}
 	optLength := flag.Float64("length", 60.0, "Time-history length(sec).")
 	optDt := flag.Float64("dt", 0.01, "DT")
-	optLevel := flag.Int("level", 2, "Specify level 1 or 2.")
+	//optLevel := flag.Int("level", 2, "Specify level 1 or 2.")
+	optEnvelope := flag.String("envelope", "id", "Specify envelope function.")
 	optCsv := flag.Bool("csv", false, "Output as CSV format.")
 	optVersion := flag.Bool("version", false, "Show version.")
 	flag.Parse()
@@ -70,11 +71,16 @@ Options:
 	}
 
 	// Set envelope function
-	var env func(float64) float64
-	if *optLevel == 1 {
-		env = envelope.Level1
-	} else {
-		env = envelope.Level2
+	//var env func(float64) float64
+	//if *optLevel == 1 {
+	//	env = envelope.Level1
+	//} else {
+	//	env = envelope.Level2
+	//}
+	env := envelope.GetEnveolope(*optEnvelope)
+	if env == nil {
+		fmt.Printf("Error: Not found envelope named '%s'\n", *optEnvelope)
+		os.Exit(1)
 	}
 
 	// Synthesize a wave
