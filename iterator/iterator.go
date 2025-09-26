e797556a4df58887de363aa1cab03a99ab521d5a
takatoh <takatoh.m@gmail.com>
2025-09-27 05:28:00 +0900

    iterator/iterator.go: Rename variable: iter_limit -> iterLimit.

--- iterator/iterator.go
+++ iterator/iterator.go
@@ -14,15 +14,15 @@ type Iterator struct {
 	synthesizer *synthesizer.Synthesizer
 	inspector   *inspector.Inspector
 	tuner       *tuner.Tuner
-	iter_limit  int
+	iterLimit   int
 }
 
-func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector, tuner *tuner.Tuner, iter_limit int) *Iterator {
+func New(synthesizer *synthesizer.Synthesizer, inspector *inspector.Inspector, tuner *tuner.Tuner, iterLimit int) *Iterator {
 	p := new(Iterator)
 	p.synthesizer = synthesizer
 	p.inspector = inspector
 	p.tuner = tuner
-	p.iter_limit = iter_limit
+	p.iterLimit = iterLimit
 	return p
 }
 
@@ -36,7 +36,7 @@ func (itr *Iterator) Iterate(amp []float64) ([]float64, error) {
 		wave := seismicwave.Make("", itr.synthesizer.Dt, y)
 		if itr.inspector.Inspect(wave) {
 			return y, nil
-		} else if count == itr.iter_limit {
+		} else if count == itr.iterLimit {
 			return y, fmt.Errorf("[WARN] ITERATION LIMIT OVER: %d", count)
 		} else {
 			amp = itr.tuner.Tune(amp, wave)
