6b571507e461599182f162ecdc4ecd7affd3b70c
takatoh <takatoh.m@gmail.com>
2025-09-27 05:48:56 +0900

    tuner/tuner.go(Tuner): Has member raviable 'PSv'.

--- tuner/tuner.go
+++ tuner/tuner.go
@@ -8,25 +8,30 @@ import (
 )
 
 type Tuner struct {
-	T  []float64
-	Sa []float64
+	T   []float64
+	Sa  []float64
+	PSv []float64
 }
 
 func New(t, sa []float64) *Tuner {
 	p := new(Tuner)
 	p.T = t
 	p.Sa = sa
+	psv := make([]float64, len(sa))
+	for i := range sa {
+		w := 2.0 * math.Pi / t[i]
+		psv[i] = w * sa[i]
+	}
+	p.PSv = psv
 	return p
 }
 
 // Initial values of amplitude for synthesize
 func (tnr *Tuner) InitAmplitude(length float64) []float64 {
-	amp := make([]float64, len(tnr.Sa))
-	for i := range tnr.Sa {
-		w := 2.0 * math.Pi / tnr.T[i] // omega
-		psv := w * tnr.Sa[i]          // Sv(h=0.05)
-		psv0 := (2.0 / length) * psv  // Sv(h=0.0)
-		amp[i] = 2.0 * psv0           // initial value of amplitude
+	amp := make([]float64, len(tnr.PSv))
+	for i := range tnr.PSv {
+		psv0 := (2.0 / length) * tnr.PSv[i] // Sv(h=0.0)
+		amp[i] = 2.0 * psv0                 // initial value of amplitude
 	}
 	return amp
 }
