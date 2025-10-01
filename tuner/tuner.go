dbee13a0d341a2fab7bc4f61205ebe6d335efc1e
takatoh <takatoh.m@gmail.com>
2025-10-01 19:27:47 +0900

    tuner/tuner.go(Tune): Simplify.

--- tuner/tuner.go
+++ tuner/tuner.go
@@ -39,13 +39,9 @@ func (tnr *Tuner) InitAmplitude(length float64) []float64 {
 // Values of amplitude for next
 func (tnr *Tuner) Tune(currAmp []float64, currWave *seismicwave.Wave) []float64 {
 	resp := response.Spectrum(currWave, tnr.T, 0.05)
-	currSa := make([]float64, len(resp))
-	for i := range resp {
-		currSa[i] = resp[i].Sa
-	}
-	amp := make([]float64, len(currAmp))
+	amp := make([]float64, len(resp))
 	for i := range amp {
-		amp[i] = currAmp[i] * tnr.Sa[i] / currSa[i]
+		amp[i] = currAmp[i] * tnr.Sa[i] / resp[i].Sa
 	}
 	return amp
 }
